6fb6552cf151bf684ab9898e23e062606e2c5f74
Thukten Singye <thuktensingye2163@gmail.com>
2023-05-18 01:45:00 +0600

    Added online right sidebar to display online user

---
+++ src/components/online/OnlineDoctor.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './OnlineDoctor.css'
+function OnlineDoctor() {
+  return (
+    <div className='onlineDoctor'>
+        onlineDoctor
+    </div>
+  )
+}
+
+export default OnlineDoctor
