7f46316769e878da8bad8c6404f67a40c9ef846f
Thukten Singye <thuktensingye2163@gmail.com>
2023-05-28 17:18:28 +0600

    created appointment context

---
+++ src/context/AppointmentContext.js
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import {createContext, useEffect} from 'react'
+
+export const AppointmentContext = createContext()
+
+export const AppointmentProvider = ({children}) =>{
+
+    const [showAppointmentForm, setShowAppointmentForm] = useState(false)  // state to display appointment form
+    const [appointment,setAppointment] = useState([]) // state to hold appointment data
+
+    const toggleAppointmentForm = () =>{
+        setShowAppointmentForm(!showAppointmentForm)
+    }
+    //  let skip for appointment
+
+    const appointmentContext = {
+        showAppointmentForm,
+        toggleAppointmentForm, 
+        appointment
+    }
+    return(
+        <AppointmentContext.Provider value={appointmentContext}>
+            {children}
+        </AppointmentContext.Provider>
+    )
+
+}
