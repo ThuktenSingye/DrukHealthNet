3f6a6b67a77f0087af8e900cb89ca47011d04122
Thukten Singye <thuktensingye2163@gmail.com>
2023-05-30 19:04:42 +0600

    change appointment context code

--- src/context/AppointmentContext.js
+++ src/context/AppointmentContext.js
@@ -16,6 +16,7 @@ export const AppointmentProvider = ({children}) =>{
     const appointmentContext = {
         showAppointmentForm,
         toggleAppointmentForm, 
+        setAppointment,
         appointment
     }
     return(
