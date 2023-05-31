6317e268eacc95b53145619f5ddbf44645982379
Thukten Singye <thuktensingye2163@gmail.com>
2023-05-31 16:21:30 +0600

    remove collection fetching# replce with hoook

--- src/components/appointment/Doctor.js
+++ src/components/appointment/Doctor.js
@@ -11,18 +11,13 @@ import DensityMediumIcon from '@mui/icons-material/DensityMedium';
 import Collapse from '@mui/material/Collapse';
 import ExpandLessSharpIcon from '@mui/icons-material/ExpandLessSharp';
 import ExpandMoreSharpIcon from '@mui/icons-material/ExpandMoreSharp';
-import { useEffect } from 'react';
-import {projectFirestore} from '../../firebase/config'
-import {collection, onSnapshot} from 'firebase/firestore'
-
-
+import useFetch from '../../hooks/useFetch';
+// import { useEffect } from "react";
+// import { projectFirestore } from '../../firebase/config';
+// import {collection, onSnapshot} from 'firebase/firestore'
 function Doctor() {
   const [showAll, setShowAll] = useState(false)
   const [grid, setGrid] = useState(true)
-  const [doctor, setDoctors] = useState([])
-  const [error, setError] = useState(null)
-  const [isPending, setIsPending] =useState(false)
-  const [imageUrl, setImageUrl] = useState('')
 
   const handleToggleShowAll = () =>{
     setShowAll(!showAll)
@@ -31,32 +26,8 @@ function Doctor() {
     e.preventDefault()
     setGrid(!grid)
   }
-  useEffect(()=>{
-    setIsPending(true)
-    const unsub = onSnapshot((collection(projectFirestore, 'doctors')), (snapshot)=>{
-      if(!snapshot.empty){
-        // const docs =[]
-        // const user = snapshot.user;
 
-        const docs  = snapshot.docs.map((doct)=>(
-          { id: doct.id, 
-            ...doct.data()
-          }
-        ))
-        setDoctors(docs)
-        setIsPending(false)
-      }else{
-        // throw new Error("Could not fetch data ")
-        setIsPending(false)
-        setError("Could not load data")
-      }
-    },(err)=>{
-      // throw new Error("Could not fetch")
-      setError("Error fetching doctor info")
-    })
-    return ()=> unsub
-    
-  },[])
+  const {isPending, error, data} = useFetch({collect:'doctors'})
 
 
   return (
@@ -73,7 +44,7 @@ function Doctor() {
           <>
           <Grid container spacing={{ xs: 2, md: 10 }} className='p-5' columns={{ xs: 4, sm: 8, md: 12 }}>
           {
-            doctor.slice(0,6).map((doctor, index)=>(
+            data.slice(0,6).map((doctor, index)=>(
               <Grid item xs={12} sm={6} md={3} lg={4} key={index} >
                 <DoctorCard 
                 // imgSrc={doctor.imgSrc}
@@ -90,7 +61,7 @@ function Doctor() {
           <Collapse in={showAll} style={{margin:'0', padding:'0', outlineWidth: '0'}} easing='ease-in-out' timeout={500}>
             <Grid container spacing={{ xs: 2, md: 10 }} className='p-5' columns={{ xs: 4, sm: 8, md: 12 }} >
             {
-              doctor.slice(6, doctor.length).map((doctor, index)=>(
+              data.slice(6, data.length).map((doctor, index)=>(
                 <Grid item xs={12} sm={6} md={3} lg={4} key={index} >
                   <DoctorCard 
                   // imgSrc={doctor.imgSrc} 
