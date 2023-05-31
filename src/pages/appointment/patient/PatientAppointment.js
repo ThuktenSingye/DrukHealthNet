a4fb57bcf04a0cca7c86abe285f97e0154e7fa50
Thukten Singye <thuktensingye2163@gmail.com>
2023-05-31 15:51:25 +0600

    updated patient appointment page

--- src/pages/appointment/patient/PatientAppointment.js
+++ src/pages/appointment/patient/PatientAppointment.js
@@ -5,17 +5,18 @@ import Doctor from '../../../components/appointment/Doctor'
 import DoctorFilter from '../../../components/appointment/DoctorFilter'
 import AppRequest from '../../../components/appointment/AppRequest'
 import AppointmentMade from '../../../components/appointment/AppointmentMade'
-import { useState } from 'react'
+import { AppointmentProvider } from '../../../context/AppointmentContext'
+// import { useState } from 'react'
 function PatientAppointment() {
-  const [openAppointment, setOpenAppointment] = useState(false)
-  console.log("Open Appointment", openAppointment)
-  
+
   return (
     <div className='patientApp'>
         <BannerApp />
         {/* doctor section compoent */}
         <DoctorFilter />
-        <Doctor openAppointment={openAppointment} toggleAppointmentForm ={()=> setOpenAppointment(!openAppointment)}/>
+        <AppointmentProvider>
+          <Doctor />
+        </AppointmentProvider>
         {/* appointment request */}
         <AppRequest type='request'/>
         {/* appointment section */}
